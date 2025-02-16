080b7f3233f88b2909a7de848cb46dabf89effad
Joe Theis <jatheis2@gmail.com>
2025-02-16 15:41:52 -0800

    Add Roboto font

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
 import "./reset.css";
+import "@fontsource/roboto/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
