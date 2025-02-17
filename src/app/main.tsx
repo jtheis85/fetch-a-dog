0bf0ac5eacc1f0caf0c4b436871cee34edeeabe3
Joe Theis <jatheis2@gmail.com>
2025-02-16 16:07:00 -0800

    Basic design system values global styles

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,8 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
+import "./main.css";
+import "./design-system.css";
 import "./reset.css";
 import "@fontsource/roboto/index.css";
+
+/**
+ * This file is the main entry point for the app. It should be kept clear of
+ * most concerns other than global imports and app-wide higher-order components.
+ */
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
